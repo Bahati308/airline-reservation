70b8900f81e5389439720622655aed9707f334bc
Madusha Sandaruwan <madushakv@live.com>
2022-12-12 01:55:48 +0530

    initial commit

---
+++ src/main/java/com/insidercloud/airlinereservation/AirlineReservationApplication.java
@@ -0,0 +1,13 @@
+package com.insidercloud.airlinereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insidercloud/airlinereservation/AirlineReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insidercloud.airlinereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
