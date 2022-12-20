f28b9f2b4c8bb419520441cadf0834d5cbf6e622
Madusha Sandaruwan <madushakv@live.com>
2022-12-20 11:55:11 +0530

    quickfix5 auth0 logout

--- src/main/java/com/insidercloud/airlinereservation/SecurityConfig.java
+++ src/main/java/com/insidercloud/airlinereservation/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.insidercloud.airlinereservation;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -17,20 +19,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> {
-                    try {
-                        authorize
-                                        .requestMatchers("/","/images/**").permitAll()
-                                        .anyRequest().authenticated()
-                                        .and().oauth2Login()
-                                        .and().logout()
-                                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                                        .addLogoutHandler(logoutHandler);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                );
+
+        http.authorizeHttpRequests( requests -> requests
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                )
+                    .oauth2Login()
+                    .and()
+                    .logout(logout -> logout
+                            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                            .addLogoutHandler(logoutHandler)
+                            .logoutSuccessUrl("/")
+                    );
+
         return http.build();
     }
 }
--- src/main/java/com/insidercloud/airlinereservation/controller/ProfileController.java
+++ src/main/java/com/insidercloud/airlinereservation/controller/ProfileController.java
@@ -1,18 +1,10 @@
 package com.insidercloud.airlinereservation.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Map;
 
 /**
  * Controller for requests to the {@code /profile} resource. Populates the model with the claims from the
@@ -20,8 +12,6 @@ import java.util.Map;
  */
 @Controller
 public class ProfileController {
-
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/profile")
     public String profile(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
